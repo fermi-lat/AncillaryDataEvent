c3f14a7710688732b42536ae8f41c176832a0f5f
jrb <>
2012-08-18 00:17:12 +0000

    support no-install headers on Windows

--- AncillaryDataEventLib.py
+++ AncillaryDataEventLib.py
@@ -1,7 +1,15 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AncillaryDataEvent/AncillaryDataEventLib.py,v 1.1 2008/08/15 21:42:33 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AncillaryDataEvent'])
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'AncillaryDataEvent') 
+            env.Tool('findPkgPath',package='AncillaryDataUtil') 
+            env.Tool('findPkgPath', package = 'AdfEvent') 
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'AdfEvent')         
+        env.Tool('findPkgPath', package = 'AncillaryDataUtil') 
+
 
 def exists(env):
     return 1;
--- SConscript
+++ SConscript
@@ -1,5 +1,5 @@
 # -*- python -*-
-# $Header: /nfs/slac/g/glast/ground/cvs/AncillaryDataEvent/SConscript,v 1.8 2010/06/12 17:19:39 jrb Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AncillaryDataEvent/SConscript,v 1.9 2012/01/23 19:25:40 jrb Exp $
 # Authors: N.Omodei <nicola.omodei@pi.infn.it>
 # Version: AncillaryDataEvent-01-04-04
 Import('baseEnv')
@@ -8,6 +8,7 @@ Import('packages')
 progEnv = baseEnv.Clone()
 libEnv = baseEnv.Clone()
 
+libEnv.Tool('addLinkDeps', package = 'AncillaryDataEvent', toBuild='static')
 AncillaryDataEvent = libEnv.StaticLibrary('AncillaryDataEvent',
                                           listFiles(['src/*.cxx']))
 
